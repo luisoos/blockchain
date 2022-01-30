48f749ae50393dd60d7e14f848279bb34b87a08e
Luis <81855420+luisoos@users.noreply.github.com>
2022-01-30 17:24:34 +0100

    Create Block.java

---
+++ src/Block.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Block {
+  
+  private int previousHash;
+  private String[] transactions;
+
+  private int blockHash;
+
+  public Block(int previousHash, String[] transactions) {
+    this.previousHash = previousHash;
+    this.transactions = transactions;
+
+    Object[] contents = {Arrays.hashCode(transactions), previousHash};
+    this.blockHash = Arrays.hashCode(contents);
+  }
+
+
+  // Get data
+
+  public int getPreviousHash() {
+    return previousHash;
+  }
+
+  public String[] getTransactions() {
+    return transactions;
+  }
+
+  public int getBlockHash() {
+    return blockHash;
+  }
+
+}
